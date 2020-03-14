f49a6539ac7336ae5d51ca2718fdf56c966d19c9
zalexki <haumalex@gmail.com>
2020-03-14 23:00:13 +0100

    add some docker thing

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 `./nut run` to start the project
 
 you need to install XQuartz on Mac
+
+
+# TODO:
+look at this guy work : https://github.com/giper45/docker-js/tree/master/lib
---
+++ _dev/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+  import Home from '@/pages/Home';
+
+  export default {
+    name: 'App',
+    components: {
+      Home
+    },
+  };
+</script>
+
+<style lang="scss">
+body {
+  min-width: 100%;
+  min-height: 100%;
+  background-color: #4D4D4D;
+}
+</style>
